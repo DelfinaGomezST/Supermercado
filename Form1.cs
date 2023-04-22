50116e73cd778b72e6f98110b4261924eabbd76d
GDellatorre2023 <gabriel.dellatorre@yahoo.com.ar>
2023-04-22 17:57:12 -0300

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -20,9 +20,24 @@ namespace Supermercado
         private void tabPage1_Click(object sender, EventArgs e)
         {
 
-        }
+        }        
 
         private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
